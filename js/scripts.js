979bc1846accade502c6439d06d6116073b4cce0
DANIELLE NAOMI NAKATSU <38386161892@jpmorgan.sp>
2018-01-24 20:43:24 -0200

    edit and save edited note

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var listaNotas = [];
 
+// var editando = false;
+
 //////////////////////////////////////////////////
 
 function atualizarSecao(secao) {
@@ -13,25 +15,22 @@ function atualizarSecao(secao) {
     for (var index = 0; index < listaNotas.length; index++) {
 
         if (listaNotas[index].editando == true) {
-            
-            inserirHTML += '<form class="note" onclick="editarNota(' + index + ', this.parentElement)">' +
-            '<button class="note__excluir" onclick="excluirNota(this.form.parentElement,' + index + ')">' +
-            '<i class="fa fa-times" aria-hidden="true"></i>' +
-            '</button>' +
-            '<input class="note__title" type="text" name="title" placeholder="Título" value="' + listaNotas[index].titulo + '" autofocus />' +
-            '<textarea class="note__body" name="body" rows="5" placeholder="Criar uma nota...">' + listaNotas[index].texto + '</textarea>' +
-            '<button class="note__control" type="button" onclick="adicionarNota(this.form.title, this.form.body, this.form, this.form.nextElementSibling)"> Concluído </button>'
-            '</form>'
+
+            inserirHTML += '<form class="note note--editing">' +
+                '<input class="note__title note__title--editing" type="text" name="title" placeholder="Título" value="' + listaNotas[index].titulo + '" autofocus />' +
+                '<textarea class="note__body note__body--editing" name="body" rows="5" placeholder="Criar uma nota...">' + listaNotas[index].texto + '</textarea>' +
+                '<button class="note__control" type="button" onclick="adicionarNota(this.form.title, this.form.body, this.form, this.form.parentElement,' + index + ')"> Salvar </button>' +
+                '</form>'
 
         } else {
-            
-        inserirHTML += '<form class="note" onclick="editarNota(' + index + ', this.parentElement)">' +
-            '<button class="note__excluir" onclick="excluirNota(this.form.parentElement,' + index + ')">' +
-            '<i class="fa fa-times" aria-hidden="true"></i>' +
-            '</button>' +
-            '<h1 class="note__title">' + listaNotas[index].titulo + '</h1>' +
-            '<p class="note__body">' + listaNotas[index].texto + '</p>' +
-            '</form>'
+
+            inserirHTML += '<form class="note" onclick="editarNota(' + index + ', this.parentElement)">' +
+                '<button class="note__excluir" onclick="excluirNota(this.form.parentElement,' + index + ')">' +
+                '<i class="fa fa-times" aria-hidden="true"></i>' +
+                '</button>' +
+                '<h1 class="note__title">' + listaNotas[index].titulo + '</h1>' +
+                '<p class="note__body">' + listaNotas[index].texto + '</p>' +
+                '</form>'
         }
     }
 
@@ -43,29 +42,50 @@ function atualizarSecao(secao) {
 
 //////////////////////////////////////////////////
 
-function adicionarNota(inputTitulo, inputTexto, formulario, secao) {
-    //criar uma variável pra armazenar dados do form (nota)
+function adicionarNota(inputTitulo, inputTexto, formulario, secao, index) {
 
-    var nota = {
-        titulo: inputTitulo.value,
-        texto: inputTexto.value,
-        editando: false
-    };
+    if (listaNotas[index]) {         // não precisa colocar o == true 
 
-    //add nota dentro de uma lista de notas
+        //pegar nota
 
-    listaNotas.push(nota);
+        //trocar titulo e texto 
 
-    //atualizar a seção de notas para aparecer na tela
+        listaNotas[index].titulo = inputTitulo.value;
+        listaNotas[index].texto = inputTexto.value;
 
-    atualizarSecao(secao);
+        //voltar o editando para false
 
-    //limpar o form preenchido
+        listaNotas[index].editando = false;
 
-    formulario.reset();
+        //atualizar seção
+
+        atualizarSecao(secao);
+
+    } else {
+
+        //criar uma variável pra armazenar dados do form (nota)
+
+        var nota = {
+            titulo: inputTitulo.value,
+            texto: inputTexto.value,
+            editando: false
+        };
+
+        //add nota dentro de uma lista de notas
+
+        listaNotas.push(nota);
+
+        //atualizar a seção de notas para aparecer na tela
+
+        atualizarSecao(secao);
+
+        //limpar o form preenchido
+
+        formulario.reset();
 
 
-    // console.log(listaNotas);
+        // console.log(listaNotas);
+    }
 }
 
 //////////////////////////////////////////////////
@@ -77,16 +97,18 @@ function excluirNota(secao, index) {
 
     //atualizar a página
     atualizarSecao(secao);
-
+    
 }
 
 //////////////////////////////////////////////////
 
 function editarNota(index, secao) {
-    
+
     //clicando na nota criada para virar editavel
     listaNotas[index].editando = true;
 
     //atualizar a tela
     atualizarSecao(secao);
 }
+
+//////////////////////////////////////////////////
