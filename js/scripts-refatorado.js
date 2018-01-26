41114ce99727d224fe7103270d0e75d393621d82
camilaibs <camilaibs@gmail.com>
2018-01-25 22:32:25 -0200

    Finalizada edição de nota

--- js/scripts-refatorado.js
+++ js/scripts-refatorado.js
@@ -18,7 +18,7 @@ var notas = {
         this.lista[posicao].editando = true;
         atualizarSecao(secao);
     },
-    salva: function(posicao, titulo, texto, secao) {
+    atualiza: function(titulo, texto, posicao, secao) {
         this.lista[posicao].titulo = titulo;
         this.lista[posicao].texto = texto;
         this.lista[posicao].editando = false;
@@ -30,17 +30,23 @@ var notas = {
 function atualizarSecao(secao) {
     var conteudoSecao = "";
 
-    for (var posicao=0; posicao < notas.lista.length; posicao++) {
+    for (var posicao = 0; posicao < notas.lista.length; posicao++) {
         if (notas.lista[posicao].editando) {
-            // template input + textarea
+            conteudoSecao += '<form class="note">'+
+                                '<input class="note__title" type="text" name="titulo" value="' + notas.lista[posicao].titulo + '" placeholder="Título">'+
+                                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + notas.lista[posicao].texto +'</textarea>'+
+                                '<button class="note__control" type="button" onclick="atualizaNota(this.form.titulo, this.form.texto, this.form, this.form.parentElement, ' + posicao +')">'+
+                                    'Concluído'+
+                                '</button>'+
+                             '</form>';
         } else {
             conteudoSecao += '<form class="note" onclick="editaFormulario(' + posicao + ', this.parentElement)">'+
-                                '<button class="note__control" type="button" onclick="removerNota(' + posicao + ', this.form.parentElement)">'+
+                                '<button class="note__control" type="button" onclick="removerNota(event, ' + posicao + ', this.form.parentElement)">'+
                                     '<i class="fa fa-times" aria-hidden="true"></i>'+
                                 '</button>'+
                                 '<h1 class="note__title">' + notas.lista[posicao].titulo + '</h1>'+
                                 '<p class="note__body">' + notas.lista[posicao].texto + '</p>'+
-                            '</form>';
+                             '</form>';
         }
     }
 
@@ -56,10 +62,19 @@ function adicionarNota(inputTitulo, textareaTexto, formulario, secao) {
     formulario.reset();
 }
 
-function removerNota(posicao, secao) {
+function atualizaNota(inputTitulo, textareaTexto, formulario, secao, posicao) {
+    var titulo = inputTitulo.value;
+    var texto = textareaTexto.value;
+
+    notas.atualiza(titulo, texto, posicao, secao);
+}
+
+function removerNota(evento, posicao, secao) {
+    evento.stopPropagation();
     notas.remove(posicao, secao);
 }
 
 function editaFormulario(posicao, secao) {
     notas.edita(posicao, secao);
+    return false;
 }
