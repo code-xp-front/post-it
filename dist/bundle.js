f658bd0c14da7924cd7edd0d66ceb3770052d940
Camila Belo <camilaibs@gmail.com>
2018-02-07 15:05:32 -0200

    Revisão

--- dist/bundle.js
+++ dist/bundle.js
@@ -264,16 +264,47 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Nota = function Nota(novoTitulo, novoTexto) {
-    _classCallCheck(this, Nota);
+var Nota = function () {
+    function Nota(novoTitulo, novoTexto) {
+        _classCallCheck(this, Nota);
 
-    // modificadores visibilidade
-    this.titulo = novoTitulo;
-    this.texto = novoTexto;
-    this.editando = false;
-};
+        this._titulo = novoTitulo;
+        this._texto = novoTexto;
+        this._editando = false;
+    }
+
+    _createClass(Nota, [{
+        key: "titulo",
+        get: function get() {
+            return this._titulo;
+        },
+        set: function set(novoTitulo) {
+            this._titulo = novoTitulo;
+        }
+    }, {
+        key: "texto",
+        get: function get() {
+            return this._texto;
+        },
+        set: function set(novoTexto) {
+            this._texto = novoTexto;
+        }
+    }, {
+        key: "editando",
+        get: function get() {
+            return this._editando;
+        },
+        set: function set(novoEditando) {
+            this._editando = novoEditando;
+        }
+    }]);
+
+    return Nota;
+}();
 
 exports.default = Nota;
 
--- src/nota.js
+++ src/nota.js
@@ -1,8 +1,31 @@
 export default class Nota {
     constructor(novoTitulo, novoTexto) {
-        // modificadores visibilidade
-        this.titulo = novoTitulo;
-        this.texto = novoTexto;
-        this.editando = false;
+        this._titulo = novoTitulo;
+        this._texto = novoTexto;
+        this._editando = false;
+    }
+
+    get titulo() {
+        return this._titulo;
+    }
+
+    set titulo(novoTitulo) {
+        this._titulo = novoTitulo;
+    }
+
+    get texto() {
+        return this._texto;
+    }
+
+    set texto(novoTexto) {
+        this._texto = novoTexto;
+    }
+
+    get editando() {
+        return this._editando;
+    }
+
+    set editando(novoEditando) {
+        this._editando = novoEditando;
     }
 }
