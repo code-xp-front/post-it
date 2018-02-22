8230bcf4e8e847db7b5f2e93ef933bde977b26fd
daninaomi <daninaomi93@gmail.com>
2018-02-22 16:27:59 -0300

    trying to do reducers

--- reducers.js
+++ reducers.js
@@ -1,18 +1,43 @@
+import { 
+  ADD_NOTE,
+  REMOVE_NOTE,
+  EDIT_NOTE,
+  SAVE_NOTE,
+  adicionarNota, 
+  removerNota, 
+  editarNota, 
+  salvarNota 
+} from './actions'
+
+import NovaLista from './src/ClasseNovaLista'
+
+const initialState = {
+  adicionarNota,
+  listaNotas: []
+}
+
 function todoApp(state = initialState, action) {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
+    case ADD_NOTE:
       return Object.assign({}, state, {
-        visibilityFilter: action.filter
-      })
-    case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
+        adicionarNota: listaNotas.push(),
+        listaNotas: [
+          action.titulo,
+          action.texto,
+          action.editando = false
         ]
+      })
+    case REMOVE_NOTE:
+      return Object.assign({}, state, {
+        removerNota
+      })
+    case EDIT_NOTE:
+      return Object.assign({}, state, {
+        editarNota
+      })
+    case SAVE_NOTE:
+      return Object.assign({}, state, {
+        salvarNota
       })
     default:
       return state
