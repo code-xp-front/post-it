8f2dd49fb60b5523a97de872efb36bdfbf484dc9
camilaibs <camilaibs@gmail.com>
2018-02-27 00:52:06 -0300

    Adicionado Head

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import Navbar from '../navbar'
 import Container from '../container'
 import './layout.css'
@@ -7,6 +8,10 @@ import 'normalize.css'
 
 const Layout = ({ children, ...props }) => (
     <React.Fragment>
+        <Head>
+            <title>Post-It</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        </Head>
         <Navbar />
         <Container {...props}>
             {children}
